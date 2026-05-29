11d001088536f89d1d6c804ddfcdd1494e8d831d
Arseniy239 <arsenzagorodniu13@gmail.com>
2026-05-29 13:58:18 +0300

    fix: update next config for static export and add prod api url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL ?? 'http://127.0.0.1:8000'}/api/:path*`,
-      },
-    ];
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
 };
-
-nextConfig.trailingSlash = true;
 
 export default nextConfig;
